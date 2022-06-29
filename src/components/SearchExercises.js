96893dc1ab861fce40359e0f7f9127818468f368
kamoliddin1234 <vip.abdujamilov@gmail.com>
2022-06-29 17:15:03 +0500

    Search UI

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,5 +1,64 @@
+import { Box, Stack, Typography, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      console.log(exercisesData);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="49px"
+        textAlign="center"
+      >
+        Awesome Exercises <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "1170px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "56px",
+            position: "absolute",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+      <Box></Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
